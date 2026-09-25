919d26c44eed06f41a6dc687eec3b29f2a448014
folfoa <humanangry200@gmail.com>
2026-09-25 21:08:17 +0800

    Update urlshorten.uc.mjs

--- urlshorten.uc.mjs
+++ urlshorten.uc.mjs
@@ -1,26 +1,6 @@
-(() => {
-    if (typeof BrowserUIUtils === "undefined") {
-        console.error("BrowserUIUtils is not available");
-        return;
-    }
-
-    BrowserUIUtils.trimURL = function (aURL) {
-        let url = this.removeSingleTrailingSlashFromURL(aURL);
-
-        // Remove http:// or https://
-        url = url.replace(/^https?:\/\//, "");
-
-        // Remove Zen's bracketed path:
-        // chatgpt.com[/c/12345]
-        url = url.replace(/\[.*?\]$/, "");
-
-        // Remove a normal path:
-        // chatgpt.com/c/12345 -> chatgpt.com
-        url = url.split("/")[0];
-
-        // Remove query and hash
-        url = url.split("?")[0].split("#")[0];
-
-        return url;
-    };
-})();
+BrowserUIUtils.trimURL = function trimURL(aURL) {
+    let url = this.removeSingleTrailingSlashFromURL(aURL);
+    return url.startsWith("http://") || url.startsWith("https://")
+        ? url.split('/')[2]
+        : url;
+}
